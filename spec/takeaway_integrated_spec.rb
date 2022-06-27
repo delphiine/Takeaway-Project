15dfc6ded43376a477c23ba9018cc6f9b78d5b0b
delphine <delphine.mubiligi@gmail.com>
2022-06-27 20:31:01 +0100

    integrated testing adding multiple items to menu

--- spec/takeaway_integrated_spec.rb
+++ spec/takeaway_integrated_spec.rb
@@ -1,13 +1,24 @@
 require 'takeaway'
-require 'menu'
+require 'menu_item'
 
 RSpec.describe Takeaway do
     context "Show the menu" do
         it "Returns 1 menu item" do
-            menu_item = Menu.new("1", "Dish", "5")
-            takeaway = Takeaway.new(menu_item)
+            takeaway = Takeaway.new
+            menu_item = MenuItem.new("1", "Dish", "5")
+            takeaway.add_to_menu(menu_item)
             result = takeaway.view_menu
             expect(result).to eq "1. Dish - £5"
+        end
+
+        it "Returns multiple menu items" do
+            takeaway = Takeaway.new
+            menu_item_1 = MenuItem.new("1", "Dish 1", "5")
+            menu_item_2 = MenuItem.new("2", "Dish 2", "7")
+            takeaway.add_to_menu(menu_item_1)
+            takeaway.add_to_menu(menu_item_2)
+            result = takeaway.view_menu
+            expect(result).to eq "1. Dish 1 - £5\n2. Dish 2 - £7"
         end
     end
 end
