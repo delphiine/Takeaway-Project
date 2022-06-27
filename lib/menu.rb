eaaf44a1cb4f3a3d72f5c6ca76262485fecb6fec
delphine <delphine.mubiligi@gmail.com>
2022-06-27 19:28:23 +0100

    added Menu class, initialize method and item number method

---
+++ lib/menu.rb
@@ -0,0 +1,11 @@
+class Menu
+    def initialize(item_num, dish, price)
+      @item_num = item_num
+      @dish = dish
+      @price = price
+    end
+  
+    def item_num
+        return @item_num
+    end
+  end
