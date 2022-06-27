0c48920fab6bdb51a3c227a851f757d196d78576
delphine <delphine.mubiligi@gmail.com>
2022-06-27 21:25:18 +0100

    added 'view_basket' and 'add_to_basket' methods

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,6 +1,7 @@
 class Takeaway
     def initialize
         @menu = []
+        @basekt = []
     end
 
     def view_menu
@@ -11,4 +12,15 @@ class Takeaway
         @menu << ("#{item.item_num}. #{item.dish} - £#{item.price}")
     end
 
+    def view_basekt
+        @basekt.join("\n")
+    end
+
+    def add_to_basekt(num)
+        @menu.each do |item|
+            if item[0] == num
+                @basekt << item
+            end
+        end     
+    end
 end
