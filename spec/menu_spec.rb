913ce58f4004e9cd0242ef9d1423a6ed4c6d09d4
delphine <delphine.mubiligi@gmail.com>
2022-06-27 19:33:29 +0100

    unit testing price method for 'Menu' class

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -6,4 +6,16 @@ RSpec.describe Menu do
         result = menu_item.item_num
         expect(result).to eq "1"
     end
+
+    it "Returns the name of a dish" do
+        menu_item = Menu.new("1.", "Dish", "5")
+        result = menu_item.dish
+        expect(result).to eq "Dish"
+    end
+
+    it "Returns the price of a dish" do
+        menu_item = Menu.new("1.", "Dish", "5")
+        result = menu_item.price
+        expect(result).to eq "5"
+    end
 end
