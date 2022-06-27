13073df0b1a49da28f006a23882a3d94e484847f
delphine <delphine.mubiligi@gmail.com>
2022-06-27 21:25:18 +0100

    integrated testing adding item to basket

--- spec/takeaway_integrated_spec.rb
+++ spec/takeaway_integrated_spec.rb
@@ -2,7 +2,7 @@ require 'takeaway'
 require 'menu_item'
 
 RSpec.describe Takeaway do
-    context "Show the menu" do
+    context "Shows the menu" do
         it "Returns 1 menu item" do
             takeaway = Takeaway.new
             menu_item = MenuItem.new("1", "Dish", "5")
@@ -13,12 +13,23 @@ RSpec.describe Takeaway do
 
         it "Returns multiple menu items" do
             takeaway = Takeaway.new
-            menu_item_1 = MenuItem.new("1", "Dish 1", "5")
-            menu_item_2 = MenuItem.new("2", "Dish 2", "7")
+            menu_item_1 = MenuItem.new("1", "Dish_1", "5")
+            menu_item_2 = MenuItem.new("2", "Dish_2", "7")
             takeaway.add_to_menu(menu_item_1)
             takeaway.add_to_menu(menu_item_2)
             result = takeaway.view_menu
-            expect(result).to eq "1. Dish 1 - £5\n2. Dish 2 - £7"
+            expect(result).to eq "1. Dish_1 - £5\n2. Dish_2 - £7"
+        end
+    end 
+
+    context "Shows the basket" do
+        it "Returns the basket with 1 item" do
+            takeaway = Takeaway.new
+            menu_item = MenuItem.new("1", "Dish", "5")
+            takeaway.add_to_menu(menu_item)
+            takeaway.add_to_basekt("1")
+            result = takeaway.view_basekt
+            expect(result).to eq "1. Dish - £5"
         end
     end
 end
