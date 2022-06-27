40f8965cc62688ccdcc7dbf23b77ece898ec98d0
delphine <delphine.mubiligi@gmail.com>
2022-06-27 20:43:26 +0100

    unit testing 'Takeaway' class for viewing a menu with 2 items

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -9,6 +9,16 @@ RSpec.describe Takeaway do
             result = takeaway.view_menu
             expect(result).to eq "1. Dish - £5"
         end
+
+        it "Returns 1 menu item" do
+            takeaway = Takeaway.new
+            fake_item_1 = double(:fake_item, item_num: "1", dish: "Dish_1", price: "5")
+            fake_item_2 = double(:fake_item, item_num: "2", dish: "Dish_2", price: "7")
+            takeaway.add_to_menu(fake_item_1)
+            takeaway.add_to_menu(fake_item_2)
+            result = takeaway.view_menu
+            expect(result).to eq "1. Dish_1 - £5\n2. Dish_2 - £7"
+        end
     end
 end
  
