250df897a79c316070dc2234c91715cfc0424722
delphine <delphine.mubiligi@gmail.com>
2022-06-28 00:16:33 +0100

    testing error message after adding and then removing all items from basket

--- spec/takeaway_integrated_spec.rb
+++ spec/takeaway_integrated_spec.rb
@@ -54,28 +54,23 @@ RSpec.describe Takeaway do
             result = takeaway.view_basket
             expect(result).to eq "Your Order: \n2. Dish_2 - £7\nYour Grand Total: £7"
         end
+    end
 
-        it "Return error message 'Your basket is empty' when the basket is empty" do
+    context "Shows basket error message" do
+        it "Return 'Your basket is empty' when the basket is empty" do
             takeaway = Takeaway.new
             expect{ takeaway.view_basket }.to raise_error "Your basket is empty"
         end
-    end
 
-    # context "Remove item from basket" do
-    #     it "Returns basket after removing 1 item" do
-    #         takeaway = Takeaway.new            
-    #         menu_item_1 = MenuItem.new("1", "Dish_1", "5")
-    #         menu_item_2 = MenuItem.new("2", "Dish_2", "7")
-    #         menu_item_3 = MenuItem.new("3", "Dish_3", "10")
-    #         takeaway.add_to_menu(menu_item_1)
-    #         takeaway.add_to_menu(menu_item_2)
-    #         takeaway.add_to_menu(menu_item_3)
-    #         takeaway.add_to_basket("1")
-    #         takeaway.add_to_basket("2")
-    #         takeaway.add_to_basket("3")
-    #         takeaway.remove_from_basket("2")
-    #         result = takeaway.view_basket
-    #         expect(result).to eq "Your Order: \n1. Dish_1 - £5\n3. Dish_3 - £10\nYour Grand Total: £15"
-    #     end
-    # end
+        it "Return 'Your basket is empty' when adding and then removing all items" do
+            takeaway = Takeaway.new
+            menu_item_1 = MenuItem.new("1", "Dish_1", "5")
+            menu_item_2 = MenuItem.new("2", "Dish_2", "7")
+            takeaway.add_to_menu(menu_item_1)
+            takeaway.add_to_menu(menu_item_2)
+            takeaway.remove_from_basket(menu_item_1)
+            takeaway.remove_from_basket(menu_item_2)
+            expect{ takeaway.view_basket }.to raise_error "Your basket is empty"
+        end
+    end
 end
