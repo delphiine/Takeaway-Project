d32212b84a5a64866aa6b004a0739ba92966c072
delphine <delphine.mubiligi@gmail.com>
2022-06-27 22:32:28 +0100

    updated the 'view_basket' message and added a grand total

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -2,9 +2,10 @@ class Takeaway
     def initialize
         @menu = []
         @basekt = []
+        @grand_total = 0
     end
 
-    def view_menu
+    def view_menu 
         @menu.join("\n")
     end
 
@@ -12,8 +13,17 @@ class Takeaway
         @menu << ("#{item.item_num}. #{item.dish} - £#{item.price}")
     end
 
-    def view_basekt
-        @basekt.join("\n")
+    def view_basekt  # Returns a string
+    #     Your Order: 
+    #     1. Dish_1 - £1
+    #     2. Dish_2 - £1
+    #     Your Grand Total: £2
+        "Your Order: \n#{@basekt.join("\n")}\nYour Grand Total: £#{grand_total}"
+    end
+
+    def grand_total # Calculates the total of item prices and returns integer
+        @menu.each { |item| @grand_total += item[-1].to_i } 
+        @grand_total
     end
 
     def add_to_basekt(num)
