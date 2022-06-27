d90571e559f3ca1a0af636d2303ecba88e36e56b
delphine <delphine.mubiligi@gmail.com>
2022-06-27 21:36:42 +0100

    testing adding multiple items to a basket

--- spec/takeaway_integrated_spec.rb
+++ spec/takeaway_integrated_spec.rb
@@ -32,4 +32,18 @@ RSpec.describe Takeaway do
             expect(result).to eq "1. Dish - £5"
         end
     end
+
+    context "Shows the basket" do
+        it "Returns the basket with multiple items" do
+            takeaway = Takeaway.new            
+            menu_item_1 = MenuItem.new("1", "Dish_1", "5")
+            menu_item_2 = MenuItem.new("2", "Dish_2", "7")
+            takeaway.add_to_menu(menu_item_1)
+            takeaway.add_to_menu(menu_item_2)
+            takeaway.add_to_basekt("1")
+            takeaway.add_to_basekt("2")
+            result = takeaway.view_basekt
+            expect(result).to eq "1. Dish_1 - £5\n2. Dish_2 - £7"
+        end
+    end
 end
